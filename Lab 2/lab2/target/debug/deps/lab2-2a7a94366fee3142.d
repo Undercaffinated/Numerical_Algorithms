d6c13642fd2aea985315ee8e85e1a3693b62e4be
Undercaffinated <nathan.c.howell@proton.me>
2026-02-15 13:10:07 -0500

    Fix incorrect upload

---
+++ Lab 2/lab2/target/debug/deps/lab2-2a7a94366fee3142.d
@@ -0,0 +1,5 @@
+/home/n/Github/Numerical_Algorithms/Lab 2/lab2/target/debug/deps/lab2-2a7a94366fee3142.d: src/main.rs
+
+/home/n/Github/Numerical_Algorithms/Lab 2/lab2/target/debug/deps/lab2-2a7a94366fee3142: src/main.rs
+
+src/main.rs:
---
+++ Lab 2/lab2/target/debug/lab2.d
@@ -0,0 +1,1 @@
+/home/n/Github/Numerical_Algorithms/Lab\ 2/lab2/target/debug/lab2: /home/n/Github/Numerical_Algorithms/Lab\ 2/lab2/src/main.rs
